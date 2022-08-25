bc0ca49494ae2b6889235da34107a08bab28dd5b
Stephen Leitnick <sleitnick@gmail.com>
2022-08-25 14:55:03 -0400

    Preferred

--- src/prefer.ts
+++ src/prefer.ts
@@ -1,4 +1,4 @@
-import { UserInputService } from "@rbxts/services";
+import { GuiService, UserInputService } from "@rbxts/services";
 
 export namespace Clack {
 	/**
@@ -47,12 +47,27 @@ export function getPreferredInput(): Clack.InputType {
  *
  * This can be useful for dynamically changing UI prompts and other game elements
  * to represent the proper control schema for the user.
- * @param observer Callback function to receive the preferred input type
- * @returns Unsubscriber function, which can be called to stop observing
  *
  * ```ts
  * observePreferredInput((preferred) => print(preferred));
  * ```
+ *
+ * A common pattern is to have a switch statement on the preferred input type:
+ * ```ts
+ * observePreferredInput((preferred) => {
+ * 	switch (preferred) {
+ * 		case Clack.InputType.MouseKeyboard:
+ * 			break;
+ * 		case Clack.InputType.Touch:
+ * 			break;
+ * 		case Clack.InputType.Gamepad:
+ * 			break;
+ * 	}
+ * });
+ * ```
+ *
+ * @param observer Callback function to receive the preferred input type
+ * @returns Unsubscriber function, which can be called to stop observing
  */
 export function observePreferredInput(observer: ObserverFn): () => void {
 	observers.push(observer);
@@ -71,13 +86,36 @@ function setPreferred(preferred: Clack.InputType) {
 	observers.forEach((observer) => task.spawn(observer, preferred));
 }
 
+function determineInitialPreferred() {
+	// Try to determine preferred input from other sources.
+	if (GuiService.IsTenFootInterface()) {
+		// Prefer gamepad if running on console.
+		setPreferred(Clack.InputType.Gamepad);
+	} else {
+		// Determine preferred from currently-enabled devices.
+		if (UserInputService.KeyboardEnabled && UserInputService.MouseEnabled) {
+			setPreferred(Clack.InputType.MouseKeyboard);
+		} else if (UserInputService.TouchEnabled) {
+			setPreferred(Clack.InputType.Touch);
+		} else if (UserInputService.GamepadEnabled) {
+			setPreferred(Clack.InputType.Gamepad);
+		} else {
+			// Failed to determine input device; default to MouseKeyboard.
+			setPreferred(Clack.InputType.MouseKeyboard);
+		}
+	}
+}
+
 function determinePreferred(inputType: Enum.UserInputType) {
+	// Determine preferred input from last-used input type.
 	if (inputType === Enum.UserInputType.Touch) {
 		setPreferred(Clack.InputType.Touch);
 	} else if (inputType === Enum.UserInputType.Keyboard || inputType.Name.sub(1, 5) === "Mouse") {
 		setPreferred(Clack.InputType.MouseKeyboard);
 	} else if (inputType.Name.sub(1, 7) === "Gamepad") {
 		setPreferred(Clack.InputType.Gamepad);
+	} else if (currentPreferredInputType === undefined) {
+		determineInitialPreferred();
 	}
 }
 
